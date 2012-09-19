ba55a4b755b02aaaa1f3ce0e5db60afe48e5a8d5
nafiboss <goodnafi@gmail.com>
2012-09-19 19:09:35 +0600

    bootstrap added

--- mytestsite/settings.py
+++ mytestsite/settings.py
@@ -162,3 +162,8 @@ CACHES = {
         'LOCATION': '/tmp/news_cred_caches',
         }
 }
+
+BOOTSTRAP_BASE_URL      = 'http://twitter.github.com/bootstrap/assets/'
+BOOTSTRAP_CSS_BASE_URL  = BOOTSTRAP_BASE_URL + 'css/'
+BOOTSTRAP_CSS_URL       = BOOTSTRAP_CSS_BASE_URL + 'bootstrap.css'
+BOOTSTRAP_JS_BASE_URL   = BOOTSTRAP_BASE_URL + 'js/'
